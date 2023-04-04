db1ace3531183c4ca0f94a6ac7af819fcf6066d9
Apoorv Tiwari <apurv9950@gmail.com>
2023-04-04 19:30:29 +0530

    ⭐ Tested with adding express.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default class Index extends React.Component {
         return (
             <div className={styles.app}>
                 <header className={styles.header}>
-                    <Link href="/page3">
+                    <Link href="/ohyeah">
                         <img src="/logo.png"
                             className={styles.logo} alt="logo"
                         />
